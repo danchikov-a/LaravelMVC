1b39432c010e383b13a5aeef93bca62c44b21a5d
artem <danchikovartem@gmail.com>
2022-12-05 02:49:34 +0300

    Added testing db

--- config/database.php
+++ config/database.php
@@ -94,10 +94,10 @@ return [
         ],
 
         'testing' => [
-            'driver'    => 'sqlite',
+            'driver' => 'sqlite',
             'host' => env('127.0.0.1'),
-            'database'  => ':memory:',
-            'prefix'    => ''
+            'database' => ':memory:',
+            'prefix' => ''
         ]
     ],
 
@@ -131,7 +131,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
